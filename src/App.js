247a21ca49add05095a6724f4e475126388f752d
David Bolin <david@bolin.at>
2019-04-11 13:27:23 -0700

    removing dotenv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
+import AddBookmark from './addBookmark/addBookmark';
+import BookmarkApp from './bookmarkApp/bookmarkApp';
+
+require('dotenv').config();
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    });
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    });
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:8000/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer ${process.env.API_TOKEN}",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
   render() {
+    const page = this.state.showAddForm
+          ? <AddBookmark
+          showForm={show => this.setShowAddForm(show)}
+          handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp bookmarks={this.state.bookmarks} showForm={show => this.setShowAddForm(show)}/>; 
+
     return (
       <div className="App">
+        { page }
       </div>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  API_TOKEN: process.env.API_TOKEN
+}
